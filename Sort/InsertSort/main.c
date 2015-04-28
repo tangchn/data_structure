4b27b69029c7e5254dba59d211367d30e389ed4a
Yves Tang <tangmuye@gmail.com>
2015-04-28 23:08:59 +0800

    Update main.c

--- Sort/InsertSort/main.c
+++ Sort/InsertSort/main.c
@@ -1,4 +1,11 @@
-﻿#include <stdlib.h>
+/*************************************************************************
+	> File Name: main.c
+	> Description: 直接插入排序
+	> Author: Yves
+	> Mail: mail: me@tangye.me
+	> Created Time: 2015-4-28. 23:08:51
+ ************************************************************************/
+#include <stdlib.h>
 #include <stdio.h>
 
 #define ARRAYNUMBER 5
@@ -8,29 +15,9 @@ void Insertsort(ElemType *array,int n);
 void InitalArray(ElemType* array);
 void PrintArray(ElemType* array);
 
-/*void Insertsort(ElemType array[], int n)
-{
-	int i, j;
-	for (i = 1; i < n; i++)
-		if (array[i] < array[i - 1])
-		{
-			int temp = array[i];
-			//找到一个后面比前面小的，然后开始找位置
-			for (j = i - 1; j >= 0 && array[j] > temp; j--)//array[j]必须大于temp才是合适的位置
-            {
-                array[j + 1] = array[j];//元素开始后移
-            }
-			array[j + 1] = temp;
-		}
-		//还有一种方法,但是这种貌似效率不好
-	for(j = i - 1; j >=0 && array[j] > array[j+1]; j--)
-        {
-            int temp = array[j];
-            array[j] = array[j+1];
-            array[j+1] = temp;
-        }
-}*/
-
+/*
+*假定a[0...i-1]为有序，a[i]..为无序，从i=1开始，将a[i]插入到一个合适的位置
+*/
 void Insertsort(ElemType array[], int n)
 {
 	int i,j;
@@ -38,12 +25,19 @@ void Insertsort(ElemType array[], int n)
 	for(i = 1; i < n; i++)
 	{
 		temp = array[i];
-		for(j = i - 1; j >= 0 && array[j] > temp; j--)
+		for(j = i - 1; j >= 0 && array[j] > temp; j--)//array[j]必须大于temp才是合适的位置
 		{
-			array[j+1] = array[j];		
+			array[j+1] = array[j];//元素开始后移		
 		}
 		array[j+1] = temp;
 	}
+	//还有一种方法,但是这种貌似效率不好
+	/*for(j = i - 1; j >=0 && array[j] > array[j+1]; j--)
+        {
+            int temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
+        }*/
 }
 
 void InitalArray(ElemType* array)
