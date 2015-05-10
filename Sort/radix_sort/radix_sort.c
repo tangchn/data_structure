002ac43bd032f08f6989fdc0f728c3bb17ec93df
Yves <ty@msn.cn>
2015-05-10 23:54:47 +0800

    Update radix_sort.c

--- Sort/radix_sort/radix_sort.c
+++ Sort/radix_sort/radix_sort.c
@@ -14,13 +14,20 @@
 typedef unsigned long long int u64;
 typedef int ElemType;
 
-void Rdtsc(u64* result);
-void InitalArray(ElemType* array);
-void PrintArray(ElemType* array);
-void radix_sort_lsd(ElemType* array);
-void radix_sort_msd(ElemType* array);
+static void Rdtsc(u64* result);
+static void InitalArray(ElemType* array);
+static void PrintArray(ElemType* array);
+static void radix_sort_lsd(ElemType* array);
+static void radix_sort_msd(ElemType* array);
+static int get_digit(int n, const int i)
 
-void Rdtsc(u64* result)
+typedef struct List_node_t 
+{
+	ElemType key; //关键字
+	int next;  //下一个节点
+}List_node;
+
+static void Rdtsc(u64* result)
 {
     asm(
         "rdtsc\n\t"
@@ -28,45 +35,99 @@ void Rdtsc(u64* result)
     );
 }
 
-void InitalArray(ElemType* array)
+static void InitalArray(list_node* array)
 {
     int i;
-    for(i = 0; i < ARRAYNUMBER; i++)
+    //头指针
+    array[0].key = 0;
+    arrya[0].next = 0;
+    for(i = 1; i < ARRAYNUMBER; i++)
     {
-        array[i] = (ElemType)rand()%100;
+        array[i].key = (ElemType)rand()%1000;
+        array[i].next = 0;
     }
 }
 
-void PrintArray(ElemType* array)
+static void PrintArray(ElemType* array)
 {
-    int i;
-    for(i = 0; i < ARRAYNUMBER; i++)
+	for(i = array[0].next; i != 0; array[i].next)
+	{
+		printf("%d ",array[i].key);
+	}
+    	printf("\n");
+}
+
+static void radix_sort_lsd(List_node* array, const int d)
+{
+    int i, j, k;
+    int current, last;
+    int rear[ARRAYNUMBER], front[ARRAYNUMBER];
+    
+    for(i = 0; i < n; i++)
     {
-        printf("%d ",array[i]);
+    	array[i].next = i + 1;
     }
-    printf("\n");
+    array[ARRAYNUMBER].next = 0;
+    for(i = 0; i < d; i++)
+    {
+    	for(j = 0; j < ARRAYNUMBER; j ++)
+    	{
+    		front[j] = 0;
+    	}
+    	for(current = array[0].next; current != 0; current = array[current].next)
+    	{
+    		k = get_dit(array[current].key, i + 1);
+    		if(front[k] == 0)
+    		{
+    			front[k] = current;
+    			rear[k] = current;
+    		}else
+    		{
+    			array[rear[k]].next = current;
+    			rear[k] = current;
+    		}
+    	}
+    	j = 0;
+    	while(front[j] == 0)
+    	{
+    		j++;
+    	}
+    	array[0].link = current = front[j];
+    	last = rear[j];
+    	for(j = j + 1; j < ARRAYNUMBER; j++)
+    	{
+    		if(front[j] != 0)
+    		{
+    			array[last].next = front[j];
+    			last = rear[j]
+    		}
+    	}
+    	array[last].next = 0;
+    }
 }
 
-
-void radix_sort_lsd(ElemType* array)
+static int get_digit(ElemType n, const int i)
 {
-    int i,j,k;
-    ElemType temp;
+	int j;
+	for(j = 1; j < i; j++)
+	{
+		n /= 10;
+	}
+	return n % 10;
 }
-
 
 int main(void)
 {
     int i;
     u64 begin,end;
-    ElemType* array = (int*)malloc(sizeof(int)*ARRAYNUMBER);
+    List_node* array = (List_node*)malloc(sizeof(List_node)*(ARRAYNUMBER + 1));
     InitalArray(array);
     PrintArray(array);
 
     Rdtsc(&begin);
-    radix_sort_lsd(array);
+    radix_sort_lsd(array, 3);
     Rdtsc(&end);
-    printf("简单选择排序后的数组是:");
+    printf("基数排序后的数组是:");
     PrintArray(array);
     printf("花费时间为:%llu \n",end - begin);
     
