f283d8fbf7217110c57f9e19ac4f1f0265414490
Yves <ty@msn.cn>
2015-05-11 00:21:45 +0800

    Update radix_sort.c

--- Sort/radix_sort/radix_sort.c
+++ Sort/radix_sort/radix_sort.c
@@ -50,11 +50,11 @@ static void InitalArray(List_node* array)
 static void PrintArray(List_node* array)
 {
 	int i;
-	for(i = array[0].next; i != 0; array[i].next)
+	for(i = array[0].next; i != 0; i= array[i].next)
 	{
 		printf("%d ",array[i].key);
 	}
-    	printf("\n");
+    printf("\n");
 }
 
 static void radix_sort_lsd(List_node* array, const int d)
@@ -119,12 +119,12 @@ int main(void)
     InitalArray(array);
     PrintArray(array);
 
-    Rdtsc(&begin);
+    /*Rdtsc(&begin);
     radix_sort_lsd(array, 3);
     Rdtsc(&end);
     printf("基数排序后的数组是:");
     PrintArray(array);
     printf("花费时间为:%llu \n",end - begin);
-    
+    */
     return 0;
 }
