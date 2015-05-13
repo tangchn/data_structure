576897a2ab1e9e9a252bb8dfff4bcb0efd7f4b75
yves <ty@msn.cn>
2015-05-13 23:42:16 +0800

    re-write strlen

---
+++ String/strlen.c
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: strlen.c
+	> Author: Yves
+	> E-mail: ty@msn.cn
+	> Created Time: 2015-05-13. 22:59:19
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+static size_t strlen(const char* str);
+
+int main(void)
+{
+	char* test_string = "Hello Yves!";
+	printf("The size of 'test_string' is: %d\n",strlen(test_string));
+}
+
+static size_t strlen(const char* str)
+{
+	const char *s;
+	s = str;
+	while(*s)
+	{
+		s++;
+	}
+	return (s - str);
+}
