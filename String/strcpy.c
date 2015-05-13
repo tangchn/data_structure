9e823f8933549f02cd1186a517257c50561087f6
Yves <ty@msn.cn>
2015-05-14 00:04:48 +0800

    Update strcpy.c

--- String/strcpy.c
+++ String/strcpy.c
@@ -8,3 +8,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+static char* strcpy(const char* src, char* dst);
+
+int main(void)
+{
+	char* src = "Hello Yves";
+	size_t len = strlen(test_string);
+	char* dst = (char*)malloc(len*sizeof(char));
+	dst = strcpy(src,dst);
+	printf("The content 'dst' is: %s\n",dst);
+}
+
+static char* strcpy(const char* src, char* dst)
+{
+	assert(dst != NULL && src != NULL);
+	char* cp = dst;
+	while((*(++dst) = *(++src)) != '\0');
+	return dst;
+}
