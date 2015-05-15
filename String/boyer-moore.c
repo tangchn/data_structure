7838cf1a186712e8f9b3ea8b72bd8d2c7a874df4
Yves <ty@msn.cn>
2015-05-15 18:17:01 +0800

    Update boyer-moore.c

--- String/boyer-moore.c
+++ String/boyer-moore.c
@@ -2,9 +2,38 @@
 	> File Name: boyer-moore.c
 	> Author: Yves
 	> E-mail: ty@msn.cn
-	> Created Time: 2015-05-14. 00:33:43
+	> Created Time: 2015-05-16. 16:13:43
  ************************************************************************/
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+
+const int ASCII_SIZE = 256;
+
+/*字符串中每个字符最靠右的位置*/
+static void rightmost(const char* pattern, const size_t pLen, int* right)
+{
+	size_t i;
+	for(i = 0; i < ASCII_SIZE; i++)
+	{
+		right[i] = m;
+	}
+	for(i = 0; i < pLen; i++)
+	{
+		right[(unsigned char)pattern[i]] = m - 1 - i;
+	}
+}
+
+int main(void)
+{
+	const char target[] = "abaaabbababbababba;abaababbababaabba";
+	const char pattern[] = "ababbaba";
+	int right[ASCII_SIZE];
+	const size_t tLen = strlen(target);
+	const size_t pLen = strlen(pattern);
+	
+	rightmost(pattern, pLen, right);
+}
+
 
