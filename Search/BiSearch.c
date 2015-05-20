26b55fb034fccd1dfc12953db86678373aa85404
Yves <tangye@hotmail.com>
2015-05-20 11:41:34 +0800

    Update BiSearch.c

--- Search/BiSearch.c
+++ Search/BiSearch.c
@@ -9,19 +9,25 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+const int ARRAY_NUMBER = 30;
+typedef int ElemType;
 
-static int BiSearch(const int* array, const int n, const int key);
-static void QuickSort(int* array, const int n);
+static int BiSearch(const ElemType* array, const int n, const ElemType key);
+static void QuickSort(ElemType *array, size_t left, size_t right);
+static void InitalArray(ElemType* array);
+static void PrintArray(ElemType* array);
 
 int main(void)
 {
-	int array[10] = {1,7,9,23,33,55,67,89,233,654};
-	int key = 55;
+	ElemType array[ARRAY_NUMBER];
+	InitalArray(array);
+	QuickSort(array, 0, ARRAY_NUMBER - 1);
+	ElemType key = 55;
 	printf("%d is the %dth element of the array.\n", key, 1 + BiSearch(array, 10, key));
 	return 0;
 }
 
-static int BiSearch(const int* array, const int n, const int key)
+static int BiSearch(const ElemType* array, const int n, const ElemType key)
 {
 	if(key > array[n - 1])
 	{
@@ -47,7 +53,59 @@ static int BiSearch(const int* array, const int n, const int key)
 	return -1;
 }
 
-static void QuickSort(int* array, const int n)
+static void QuickSort(ElemType *array, size_t left, size_t right)
 {
+    size_t i,j;
+    ElemType temp, base;
+    base = array[left];
+    i = left;
+    j = right;
+    if(left > right)
+    {
+        return;
+    }
+    while(i < j)
+    {
+        //顺序很重要，要先从右边开始找
+        while(array[j] >= base && j > i)
+        {
+            j--;
+        }
+        while(array[i] <= base && i < j)
+        {
+            i++;
+        }
+        //交换两个数在数组中的位置
+        if(i < j)
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp; 
+        }
+    }
+    //最终将基准数归位
+    array[left] = array[i];//j先行保证了此时a[i]是小于temp的，因此也可以是array[left] = array[j];
+    array[i] = base;
+    
+    QuickSort(array,left,i-1);//继续处理左边的，这里是一个递归的过程
+    QuickSort(array,i+1,right);//继续处理右边的 ，这里是一个递归的过程
+}
 
+static void InitalArray(ElemType* array)
+{
+    size_t i;
+    for(i = 0; i < ARRAYNUMBER; i++)
+    {
+        array[i] = (int)rand()%100;
+    }
+}
+
+static void PrintArray(ElemType* array)
+{
+    size_t i;
+    for(i = 0; i < ARRAYNUMBER; i++)
+    {
+        printf("%d ",array[i]);
+    }
+    printf("\n");
 }
